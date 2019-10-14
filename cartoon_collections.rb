9b9a8bf0ac224b9dd7a596748f8f9be9ffef5c2a
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-14 02:59:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  if array.any?{|word|word.length > 4
+  if array.any?{|word|word.length > 4}
+    return true 
+  
   
 end
 
