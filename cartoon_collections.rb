ab1b6c88d8b045bb2a522281852057b931d2ee61
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-14 02:30:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index{|item, index|puts #{i
+  array.each_with_index{|item, index|puts "#{index
 end
 
 def summon_captain_planet# code an argument here
