fcdab5d2e25e347521b546a04080a05008e26349
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-14 03:14:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.reduce|
+  if array.reduce{|array|array.include("chedder","gouda","camembert")
   
   
 end
