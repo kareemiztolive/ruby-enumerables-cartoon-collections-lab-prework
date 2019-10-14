984d108f2b83998d56375fefcd35e7ac60e9edfe
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-14 03:12:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,9 @@ def long_planeteer_calls(array)
   
 end
 
-def find_the_cheese()
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.reduce|
+  
+  
 end
